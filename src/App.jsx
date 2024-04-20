0667c97e3f2532933aab21ae05b241457daa7abf
LabHasar <brlabhasar1@gmail.com>
2024-04-19 21:12:00 -0300

    empezando Projetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/section/Navbar";
 import Presentation from "./components/section/Presentation";
+import Projects from "./components/section/Projects";
 
 function App() {
   return(
     <div>
       <Navbar/>
       <Presentation/>
+      <Projects/>
     </div> 
   ) 
 }
--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -7,3 +7,5 @@ function Projects(){
         </div>
     )
 }
+
+export default Projects
