f543dfb7a05607cebda89342a924d2c971040923
LabHasar <brlabhasar1@gmail.com>
2024-04-19 21:01:32 -0300

    presentation / navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/section/Navbar";
+import Presentation from "./components/section/Presentation";
+
+function App() {
+  return(
+    <div>
+      <Navbar/>
+      <Presentation/>
+    </div> 
+  ) 
+}
+
+export default App;
---
+++ src/components/section/Projects.jsx
@@ -0,0 +1,9 @@
+import sx from './Projects.module.css'
+
+function Projects(){
+    return(
+        <div className={sx.projects}>
+            <h1>Projetos</h1>
+        </div>
+    )
+}
