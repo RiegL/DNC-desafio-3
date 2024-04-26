d67eff50d09f38ab2ccdba18fb276b68e07e00ea
LabHasar <brlabhasar1@gmail.com>
2024-04-25 21:20:44 -0300

    atualizado footer

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -6,14 +6,18 @@ import { FaFigma, FaLinkedin, FaGithub } from "react-icons/fa";
 function Footer(){
     return(
         <div className={sx.footer}>
+            <div className={sx.contacts}>
             <p>Meu contato:<br/>47 99600-5026</p>
             <p>Email:<br/>leoorieg@gmail.com</p> 
-        <ul>
-            <li><a href='https://github.com/RiegL' target='_blank'><FaGithub size={30}/></a></li>
-            <li><a href='https://www.linkedin.com/in/l%C3%A9o-rieg-58b64514a/' target='_blank'><FaLinkedin size={30}/></a></li>
-            <li><a target='_blank'><FaFigma size={30}/></a></li> 
-        </ul>
-       
+            </div>
+
+        <div>
+            <ul>
+                <li><a href='https://github.com/RiegL' target='_blank'><FaGithub size={30}/></a></li>
+                <li><a href='https://www.linkedin.com/in/l%C3%A9o-rieg-58b64514a/' target='_blank'><FaLinkedin size={30}/></a></li>
+                <li><a target='_blank'><FaFigma size={30}/></a></li> 
+            </ul>
+        </div>
       </div>
     )
 }
